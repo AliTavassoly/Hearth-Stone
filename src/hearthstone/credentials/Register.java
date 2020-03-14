f269d62a4af72e7f2038dfe15b74a22cc8cc1ff8
AliTavassoly <tavassolyali@gmail.com>
2020-03-14 04:08:56 +0330

    password1 to password

--- src/hearthstone/credentials/Register.java
+++ src/hearthstone/credentials/Register.java
@@ -1,12 +1,12 @@
 package hearthstone.credentials;
 
 public class Register {
-    void register(String username, String password1, String repeat){
+    void register(String username, String password, String repeat){
         if(Login.users.containsKey(username)){
             System.err.println("Username already exist !");
             return;
         }
-        if(!password1.equals(repeat)){
+        if(!password.equals(repeat)){
             System.out.println("Passwords does not match !");
             return;
         }
