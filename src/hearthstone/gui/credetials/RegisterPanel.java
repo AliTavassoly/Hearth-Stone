a304a17e98646f03b58e192489255fddf3f94e48
Ali.Tavassoly <tavassolyali@gmail.com>
2020-04-09 16:22:50 +0430

    LoginPanel and RegisterPanel completed (without hero type).

--- src/hearthstone/gui/credetials/RegisterPanel.java
+++ src/hearthstone/gui/credetials/RegisterPanel.java
@@ -13,6 +13,8 @@ import java.io.File;
 
 public class RegisterPanel extends JPanel {
     private JButton backButton, registerButton;
+    private JTextField nameField, userField, passField, passRepField;
+    private JLabel nameLabel, userLabel, passLabel, passRepLabel;
 
     public RegisterPanel(){
         configPanel();
@@ -26,6 +28,30 @@ public class RegisterPanel extends JPanel {
         backButton.setBorderPainted(false);
         backButton.setFocusPainted(false);
         backButton.setMargin(new Insets(2, 15 + 30, 2, 15 + 30));
+
+        nameField = new JTextField(10);
+        nameField.setBorder(null);
+
+        userField = new JTextField(10);
+        userField.setBorder(null);
+
+        passField = new JTextField(10);
+        passField.setBorder(null);
+
+        passRepField = new JTextField(10);
+        passRepField.setBorder(null);
+
+        nameLabel = new JLabel("name : ");
+        nameLabel.setForeground(Color.WHITE);
+
+        userLabel = new JLabel("username : ");
+        userLabel.setForeground(Color.WHITE);
+
+        passLabel = new JLabel("password : ");
+        passLabel.setForeground(Color.WHITE);
+
+        passRepLabel = new JLabel("repeat password : ");
+        passRepLabel.setForeground(Color.WHITE);
 
         backButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
@@ -65,15 +91,69 @@ public class RegisterPanel extends JPanel {
         setLayout(new GridBagLayout());
         GridBagConstraints grid = new GridBagConstraints();
 
-        // first row
+        // zero row
         grid.gridy = 0;
         grid.gridx = 0;
-        grid.insets = new Insets(10, 0, 10, 0);
+
+        // next row
+        grid.gridy++;
+        grid.gridx = 0;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(180, 0, 5, 0);
+        add(nameLabel, grid);
+
+        grid.gridx = 1;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(180, 0, 5, 0);
+        add(nameField, grid);
+
+        // next row
+        grid.gridy++;
+        grid.gridx = 0;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(5, 0, 5, 0);
+        add(userLabel, grid);
+
+        grid.gridx = 1;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(5, 0, 5, 0);
+        add(userField, grid);
+
+        // next row
+        grid.gridy++;
+        grid.gridx = 0;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(5, 0, 5, 0);
+        add(passLabel, grid);
+
+        grid.gridx = 1;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(5, 0, 5, 0);
+        add(passField, grid);
+
+        // next row
+        grid.gridy++;
+        grid.gridx = 0;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(5, 0, 5, 0);
+        add(passRepLabel, grid);
+
+        grid.gridx = 1;
+        grid.gridwidth = 1;
+        grid.insets = new Insets(5, 0, 5, 0);
+        add(passRepField, grid);
+
+        // next row
+        grid.gridy++;
+        grid.gridx = 0;
+        grid.gridwidth = 2;
+        grid.insets = new Insets(70, 0, 10, 0);
         add(registerButton, grid);
 
-        //second row
-        grid.gridy = 1;
+        //next row
+        grid.gridy++;
         grid.gridx = 0;
+        grid.gridwidth = 2;
         grid.insets = new Insets(10, 0, 10, 0);
         add(backButton, grid);
     }
